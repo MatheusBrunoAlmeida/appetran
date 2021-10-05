c56174b87ff89dd3788c3f5886ea25acb80fad8e
Matheus Bruno Almeida <matheusbrunoalmeida@Matheuss-MacBook-Air.local>
2021-10-05 18:10:32 -0300

    hello world

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello eTran</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    color: '#8974cf',
+    fontSize: 23
+  }
+});
