3bf85f4c52bc2bef78145004089e0e1c299e4d8d
Matheus Bruno Almeida <matheusbrunoalmeida@Matheuss-MacBook-Air.local>
2021-10-07 18:11:27 -0300

    Começando ui da tela de login com inputs e rodando no IOS e android

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello eTran</Text>
+      <View></View>
+      
+      <View style={styles.form}>
+        <TextInput style={styles.input} placeholder="Codigo do agente:" placeholderTextColor="#ACACAC"/>
+        <TextInput style={styles.input} placeholder="Senha:" placeholderTextColor="#ACACAC" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,12 +19,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: '#262626',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text:{
-    color: '#8974cf',
-    fontSize: 23
+  form:{
+
+  },
+  input:{
+    backgroundColor: '#414141',
+    marginBottom: 20,
+    width: 300,
+    padding: 12,
+    borderRadius: 6
   }
 });
